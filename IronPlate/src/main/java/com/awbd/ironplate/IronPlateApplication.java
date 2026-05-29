ff1694906da83726e6e135f0dba36f1eaa9a5b6e
Marian Danaila <marian.danaila@uber.com>
2026-05-29 18:37:48 +0200

    Add initial project setup and multi-environment configuration

---
+++ IronPlate/src/main/java/com/awbd/ironplate/IronPlateApplication.java
@@ -0,0 +1,13 @@
+package com.awbd.ironplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IronPlateApplication.class, args);
+    }
+
+}
---
+++ IronPlate/src/test/java/com/awbd/ironplate/IronPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awbd.ironplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronPlateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
